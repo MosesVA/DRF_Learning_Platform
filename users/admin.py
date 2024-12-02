cbfc1592b3790a59ea718d679085264de23734b4
Vladislav Moiseev <morninyep@gmail.com>
2024-12-02 23:03:39 +0500

    Модель User зарегистрирована в админке.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_active')
+    list_filter = ('id', 'email')
+    ordering = ('id',)
