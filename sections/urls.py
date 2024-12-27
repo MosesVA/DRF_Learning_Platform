da665e6dbf5f267bd897f1471da26ccfff3d9c52
Vladislav Moiseev <morninyep@gmail.com>
2024-12-28 00:33:57 +0500

    Написаны urls для Tests, проведена миграция

--- sections/urls.py
+++ sections/urls.py
@@ -6,7 +6,7 @@ from sections.apps import SectionsConfig
 
 from sections.views import SectionListAPIView, SectionCreateAPIView, SectionRetrieveAPIView, SectionUpdateAPIView, \
     SectionDestroyAPIView, ContentListAPIView, ContentCreateAPIView, ContentRetrieveAPIView, \
-    ContentUpdateAPIView, ContentDestroyAPIView
+    ContentUpdateAPIView, ContentDestroyAPIView, TestsListAPIView, TestsQuestionRetrieveAPIView
 
 app_name = SectionsConfig.name
 
@@ -14,7 +14,6 @@ router = DefaultRouter()
 
 urlpatterns = [
     # Section urlpatterns
-
     path('section/list/', SectionListAPIView.as_view(), name='sections_list'),
     path('section/create/', SectionCreateAPIView.as_view(), name='section_create'),
     path('section/<int:pk>/', SectionRetrieveAPIView.as_view(), name='section_detail'),
@@ -22,10 +21,13 @@ urlpatterns = [
     path('section/<int:pk>/delete/', SectionDestroyAPIView.as_view(), name='section_delete'),
 
     # Section urlpatterns
-
     path('content/list/', ContentListAPIView.as_view(), name='content_list'),
     path('content/create/', ContentCreateAPIView.as_view(), name='content_create'),
     path('content/<int:pk>/', ContentRetrieveAPIView.as_view(), name='content_detail'),
     path('content/<int:pk>/update/', ContentUpdateAPIView.as_view(), name='content_update'),
     path('content/<int:pk>/delete/', ContentDestroyAPIView.as_view(), name='content_delete'),
+
+    # Tests urlpatterns
+    path('tests/', TestsListAPIView.as_view(), name='tests_list'),
+    path('test/<int:pk>/', TestsQuestionRetrieveAPIView.as_view(), name='test'),
 ] + router.urls
