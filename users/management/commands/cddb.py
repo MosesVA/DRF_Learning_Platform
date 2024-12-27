7766bbcb23fecaca7179fa49cadc491c2e8cf30a
Vladislav Moiseev <morninyep@gmail.com>
2024-12-28 00:29:34 +0500

    Добавлена команды удаления Базы Данных

---
+++ users/management/commands/cddb.py
@@ -0,0 +1,27 @@
+import pyodbc
+
+from django.core.management import BaseCommand
+
+from config.settings import DATABASE, USER, PASSWORD, HOST, PAD_DATABASE, DRIVER
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        ConnectionString = f'''DRIVER={DRIVER};
+                               SERVER={HOST};
+                               DATABASE={PAD_DATABASE};
+                               UID={USER};
+                               PWD={PASSWORD}'''
+        try:
+            conn = pyodbc.connect(ConnectionString)
+        except pyodbc.ProgrammingError as ex:
+            print(ex)
+        else:
+            conn.autocommit = True
+            try:
+                conn.execute(fr'DROP DATABASE {DATABASE};')
+            except pyodbc.ProgrammingError as ex:
+                print(ex)
+            else:
+                print(f'База данных {DATABASE} успешно удалена!')
